214fe7d5350908e3579d49c245d797d895867b25
SANJAYAN KK <92313907+Sanjayankk6@users.noreply.github.com>
2021-10-31 14:54:53 +0000

    .

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -28,14 +28,14 @@ Bot Owner Only
 """
 
 ABOUT = """
-➥ Name :  Auto Filter V3
+➥ Name :  MANAVALAN BOTV3 
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [SJ AND MANAVALAN](
+https://t.me/joinchat/AAAAAEnTwQiS8yOTMPkWaw)
 
 ➥ Language : Python3
 
 ➥ Library : Pyrogram Asyncio 1.13.0
 
-➥ Tutorial : [Click Me](https://youtu.be/5hnYOKBzyi8)
+➥ Website : [Coming soon]
 """
